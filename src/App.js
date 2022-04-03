0d196dab53e729a7bba67aa476400c29cc07e5e6
Vasyl Luchka <vasyaluchka9@gmail.com>
2022-04-03 20:01:23 +0300

    hover-color-changed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Data from "./Data"
 import "./Components/Navbar/navbar.css"
 import "./Components/Card/card.css"
 
+
 export default function App(){
   const cards = Data.map(card => {
     return <Card 
